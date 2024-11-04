a1729758fcf8c5a755b513592c15b4eaa76eea3e
M-Mikos <mikos.marcin.m@gmail.com>
2024-11-04 15:06:45 +0100

    Add markup and styles

---
+++ index.js
@@ -0,0 +1,17 @@
+// 1. Build html
+// 1.1. Input field
+// 1.2. Output field
+// 1.3. Keyboard: Numbers, Delete, Clear, Operators (+, -, *, /, =), Comma
+// 2. Build styles
+// 3. Build model - input stack
+// 4. Build event hadling
+// 4.1. Clicking
+// 4.2. Typing
+// 4.3. Deleting
+// 4.4. Clearing
+// 4. Build computing algorithm
+// 5. Show output
+
+// 6. Extensions:
+// 6.1 Change sign
+// 6.2 Modulo
