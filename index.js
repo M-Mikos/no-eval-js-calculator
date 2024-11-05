518d2279d391389c6a088fb82253cbaa4e4024ff
M-Mikos <mikos.marcin.m@gmail.com>
2024-11-05 15:45:18 +0100

    Remove redundant comments

--- index.js
+++ index.js
@@ -1,21 +1,3 @@
-// 1. Build html
-// 1.1. Input field
-// 1.2. Output field
-// 1.3. Keyboard: Numbers, Delete, Clear, Operators (+, -, *, /, =), Comma
-// 2. Build styles
-// 3. Build model - input stack
-// 4. Build event hadling
-// 4.1. Clicking
-// 4.2. Typing
-// 4.3. Deleting
-// 4.4. Clearing
-// 4. Build computing algorithm
-// 5. Show output
-
-// 6. Extensions:
-// 6.1 Change sign
-// 6.2 Modulo
-
 // Data
 
 const expression = [];
@@ -159,7 +141,7 @@ function updateOutputElement(outputValue) {
   output.innerHTML = outputValue;
 }
 
-// Viewer controller
+// Controller
 
 function controllClick(term) {
   if (term.type === "action") {
